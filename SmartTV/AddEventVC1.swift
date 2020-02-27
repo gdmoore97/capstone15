90e63b62edb2ac3d116ad6a5d3e95d4fbf209a50
Gabriella Moore <gdmoore@mix.wvu.edu>
2020-02-27 11:26:41 -0500

    push to remote

--- SmartTV/AddEventVC1.swift
+++ SmartTV/AddEventVC1.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class AddEventVC1: UIViewController {
 
-    
+    // UI objects
     @IBOutlet weak var fullnameText: UITextField!
     @IBOutlet weak var eventnameText: UITextField!
     @IBOutlet weak var eventlocationText: UITextField!
@@ -21,6 +21,102 @@ class AddEventVC1: UIViewController {
     
     @IBAction func add_event(_ sender: Any) {
         
+        // if no text was entered
+        if fullnameText.text!.isEmpty || eventnameText.text!.isEmpty || eventlocationText.text!.isEmpty || eventdateText.text!.isEmpty {
+            
+                    // enforce red placeholders
+                      fullnameText.attributedPlaceholder = NSAttributedString(string: "who is the event for?", attributes: [NSAttributedString.Key.foregroundColor:UIColor.red])
+                      
+                      eventnameText.attributedPlaceholder = NSAttributedString(string: "event name", attributes: [NSAttributedString.Key.foregroundColor:UIColor.red])
+                      
+                      eventlocationText.attributedPlaceholder = NSAttributedString(string: "event location", attributes: [NSAttributedString.Key.foregroundColor:UIColor.red])
+                      
+                      eventdateText.attributedPlaceholder = NSAttributedString(string: "event date", attributes: [NSAttributedString.Key.foregroundColor:UIColor.red])
+            
+        } else {
+            
+            // user login
+            var u = ""
+            if let username = user!["username"] {
+                u = username as! String
+            }
+            
+            
+            // remove keyboard
+            self.view.endEditing(true)
+            
+            // ** create an event in MySQL **
+            
+            // url to php file
+            let url = URL(string: "http://smartersmarttv.com/tv_events.php")!
+            
+            // request to the file
+            var request = URLRequest(url: url)
+            
+            // method to pass date to the file
+            request.httpMethod = "POST"
+            
+            // body to be appended to url
+            let body = "eventname=\(eventnameText.text!)&eventlocation=\(eventlocationText.text!)&eventdate=\(eventdateDB.text!)&fullname=\(fullnameText.text!)&creator_username=\(u)"
+            
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            // process the request
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                if error == nil {
+                    
+                    DispatchQueue.main.async(execute: {
+                        
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                            
+                            guard let parseJSON = json else {
+                                print("Error while parsing")
+                                return
+                            }
+                            
+                            let id = parseJSON["id"]
+                            
+                            if id != nil {
+                                //print(username as Any)
+                                DispatchQueue.main.async(execute: {
+                                    let message = "You have added an event"
+                                    self.createAlert(title: "Success!", message: message)
+                                })
+                                
+                            } else {
+                                DispatchQueue.main.async(execute: {
+                                    let message = parseJSON["message"] as! String
+                                    self.createAlert(title: "Error", message: message)
+                                })
+                                return
+                            }
+                        } catch {
+                            DispatchQueue.main.async(execute: {
+                                let message = "\(error)"
+                                self.createAlert(title: "Error", message: message)
+                            })
+                            return
+                        }
+                        
+                    })
+                    
+                } else {
+                    DispatchQueue.main.async(execute: {
+                        let message = error!.localizedDescription
+                        self.createAlert(title: "Error", message: message)
+                    })
+                    return
+                    
+                }
+                
+                
+            // if unable to process the request
+                } .resume()
+        
+        
+        }
     }
     
     override func viewDidLoad() {
@@ -66,6 +162,17 @@ class AddEventVC1: UIViewController {
           
       }
     
+       // create alert controller
+     func createAlert (title: String, message: String) {
+         
+         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+         
+         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+             alert.dismiss(animated: true, completion: nil)
+         }))
+         
+         self.present(alert, animated: true, completion: nil)
+     }
     
 
 
