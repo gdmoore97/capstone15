f0fa4eb58cc148732bb2c5bc935b638035ea29a5
Gabriella Moore <gdmoore@mix.wvu.edu>
2020-02-26 21:30:21 -0500

    push to remote

--- SmartTV/AddEventVC1.swift
+++ SmartTV/AddEventVC1.swift
@@ -10,21 +10,64 @@ import UIKit
 
 class AddEventVC1: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var fullnameText: UITextField!
+    @IBOutlet weak var eventnameText: UITextField!
+    @IBOutlet weak var eventlocationText: UITextField!
+    @IBOutlet weak var eventdateText: UITextField!
+    @IBOutlet weak var eventdateDB: UILabel!
+    
+    var datePicker : UIDatePicker?
+    
+    @IBAction func add_event(_ sender: Any) {
+        
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .dateAndTime
+        
+        // sets up the labl for UI view
+        datePicker?.addTarget(self, action: #selector(AddEventVC1.dateChanged(datePicker:)), for: .valueChanged)
+        
+        // sets up the label for the db
+        datePicker?.addTarget(self, action: #selector(AddEventVC1.dateChangedDB(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(AddEventVC1.viewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        eventdateText.inputView = datePicker
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+      @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
+          view.endEditing(true)
+      }
+      
+      // function to format the label with human readable date label
+      @objc func dateChanged(datePicker : UIDatePicker) {
+          
+          let dateFormatter = DateFormatter()
+          dateFormatter.dateStyle = .full
+          dateFormatter.timeStyle = .short
+          eventdateText.text = dateFormatter.string(from: datePicker.date)
+          
+      }
+      
+      // function to format the label with db readable date label
+      @objc func dateChangedDB(datePicker : UIDatePicker) {
+          
+          let dateFormatter = DateFormatter()
+          dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+          eventdateDB.text = dateFormatter.string(from: datePicker.date)
+          
+      }
+    
+    
+
+
 
 }
