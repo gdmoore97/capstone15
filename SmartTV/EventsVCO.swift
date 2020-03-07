00fc2378fc988462b6af83801c675947b0479c84
Gabriella Moore <gdmoore@mix.wvu.edu>
2020-03-07 10:47:20 -0500

    push to remote

--- SmartTV/EventsVCO.swift
+++ SmartTV/EventsVCO.swift
@@ -80,7 +80,7 @@ class EventsVCO: UIViewController,UITableViewDelegate, UITableViewDataSource {
                if editingStyle == .delete {
                    
                    // send delete php request
-                   //deleteEvent(indexPath)
+                   deleteEvent(indexPath)
                }
     }
     
@@ -155,6 +155,86 @@ class EventsVCO: UIViewController,UITableViewDelegate, UITableViewDataSource {
            
            
        }
+    
+    @objc func deleteEvent(_ indexPath: IndexPath) {
+        
+        // shortcuts
+        let event = events[indexPath.row]
+        //let eventid = event["id"] as? Int
+        //print(eventid)
+         var eventid = 1
+         if let id = event["id"] {
+            eventid = id as! Int
+         }
+        print(eventid)
+        
+        let url = URL(string: "http://smartersmarttv.com/tv_deleteevents.php")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let body = "id=\(eventid)"
+        request.httpBody = body.data(using: String.Encoding.utf8)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            DispatchQueue.main.async(execute: {
+                
+                if error == nil {
+                    
+                    do {
+                        
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                        
+                        guard let parseJSON = json else {
+                            print("Error with parsing")
+                            return
+                        }
+                        
+                        let result = parseJSON["result"]
+                        
+                        if result != nil {
+                            self.events.remove(at: indexPath.row)
+                            self.tableV.deleteRows(at: [indexPath], with: .automatic)
+                            self.tableV.reloadData()
+                        } else {
+                            
+                            DispatchQueue.main.async(execute: {
+                                let message = parseJSON["message"] as! String
+                                self.createAlert(title: "Error", message: message)
+                                print("stop 1")
+                                
+                            })
+                            return
+                        }
+                    } catch {
+                        
+                        // there's an issue with the php file assigning a value to result, however, the php file does delete the event
+                        // there's only one way to delete the event through this method
+                        DispatchQueue.main.async(execute: {
+                           // let message = "\(error)"
+                           // self.createAlert(title: "Error", message: message)
+                           // print("stop 2")
+                            self.events.remove(at: indexPath.row)
+                            self.tableV.deleteRows(at: [indexPath], with: .automatic)
+                            self.tableV.reloadData()
+                        })
+                        return
+                    }
+                } else {
+                    
+                    DispatchQueue.main.async(execute: {
+                        let message = error!.localizedDescription
+                        self.createAlert(title: "Error", message: message)
+                        print("stop 3")
+                    })
+                    return
+                    
+                }
+            })
+            } .resume()
+    }
+    
+    
     
     
 
